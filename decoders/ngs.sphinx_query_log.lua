11b10ca30d4f0f75456ad77a43ddb4113396592f
validname <validname@users.noreply.github.com>
2014-10-15 17:52:18 +0700

    sphinx query log parser: both formats

--- decoders/ngs.sphinx_query_log.lua
+++ decoders/ngs.sphinx_query_log.lua
@@ -1,5 +1,5 @@
 --[[
-Parses the Nginx error logs based on the Nginx hard coded internal format.
+Parses Sphinx search query log (up to version 2.25).
 
 Config:
 
@@ -9,69 +9,70 @@ Config:
 - tz (string, optional, defaults to UTC):
     The conversion actually happens on the Go side since there isn't good TZ support here.
 
-*Example Heka Configuration 1*
+*Example Heka Configuration*
 
 .. code-block:: ini
 
-    [TestWebserverError]
+    [SphinxQuery]
     type = "LogstreamerInput"
-    log_directory = "/var/log/nginx"
-    file_match = 'error\.log'
-    decoder = "NGSNginxErrorDecoder"
+    log_directory = "/var/log/sphinx"
+    file_match = 'query\.log'
+    decoder = "SphinxQueryDecoder"
 
-    [NGSNginxErrorDecoder]
+    [SphinxQueryDecoder]
     type = "SandboxDecoder"
-    filename = "lua_decoders/ngs.nginx_error_log.lua"
+    filename = "lua_decoders/ngs.sphinx_query_log.lua"
 
-    [NGSNginxErrorDecoder.config]
-    type = "nginx_error_logs"
+    [SphinxQueryDecoder.config]
+    type = "sphinx_query_logs"
     tz = "Asia/Novosibirsk"
 
-Nginx pass through non-printable characters to error log. For example: let URL is 'http://host/uri?var=%0Avalue' and it's rewrited and proxied to upstream. Nginx will write to error log '..., request: "GET /uri?var=%0Avalue HTTP/1.1", upstream: "/uri?var=
-value"'. That is, line will be broken by 'line feed' character. It's proved up to version 1.2.9, but no further version was checked. In this case decoder will generate error message on each such line and will be restarted by sandbox. To eliminate messages and performance overhead, you may use log breaking by start of true line instead of 'line feed':
-
-*Example Heka Configuration 2*
-
-.. code-block:: ini
-
-    [TestWebserverError]
-    type = "LogstreamerInput"
-    log_directory = "/var/log/nginx"
-    file_match = 'error\.log'
-    decoder = "NGSNginxErrorDecoder"
-    parser_type = "regexp"
-    delimiter = "\n([0-9]{4}/[0-9]{2}/[0-9]{2} )"
-    delimiter_location = "start"
-
-    [NGSNginxErrorDecoder]
-    type = "SandboxDecoder"
-    filename = "lua_decoders/ngs.nginx_error_log.lua"
-
-    [NGSNginxErrorDecoder.config]
-    type = "nginx_error_logs"
-    tz = "Asia/Novosibirsk"
-
-*Example Heka Message*
+*Example Heka Message 1*
 
 :Timestamp: 2014-09-24 17:19:56 +0700 NOVT
-:Type: nginx_error_logs
-:Hostname: frontend6
-:Pid: 16842
+:Type: sphinx_query_logs
+:Hostname: search1
+:Pid: 23678
 :UUID: a1ee8eb0-e60b-4b81-a143-5dfe9a64b75b
-:Logger: TestWebserverError
-:Payload: delaying request, excess: 0.015, by zone \u0022common_ip_nonRU\u0022
+:Logger: SphinxQuery
+:Payload: 
 :EnvVersion:
-:Severity: 5
+:Severity: 7
 :Fields:
-    | name:"thread_id" value_type:DOUBLE value_double:0
-    | name:"connection" value_type:DOUBLE value_double:386031267
-    | name:"client" value_string:"148.251.112.123"
-    | name:"server" value_string:"ngs.ru"
-    | name:"request" value_string:"GET /path/index.php?query=string HTTP/1.1
-    | name:"subrequest" value_string:"/ssi.php"
-    | name:"upstream" value_string:"fastcgi://127.0.0.1:4006"
-    | name:"host" value_string:"ngs.ru"
-    | name:"referrer" value_string:"http://ngs.ru/"
+    | name:"total-matches" value_type:DOUBLE value_double:10
+    | name:"query_type" value_string:"SphinxQL"
+    | name:"wall-time" value_type:DOUBLE value_double:0.006
+    | name:"real-time" value_type:DOUBLE value_double:0.006
+    | name:"query_sql" value_string:"SELECT  FROM market_city68_goods GROUP BY cat_id WITHIN GROUP ORDER BY mode-0 LIMIT 0, 5000 OPTION max_matches=5000, ranker=proximity"
+    | name:"query_error" value_string:"unknown local index 'market_city68_goods' in search request"
+    | name:"io_stats" value_string:"ios=20 kb=24.0 ioms=0.200 cpums=10.2 agents=(0.003, 1.123)"
+    | name:"connection_id" value_type:DOUBLE value_double:1176853648
+
+*Example Heka Message 2*
+
+:Timestamp: 2014-09-24 17:19:56 +0700 NOVT
+:Type: sphinx_query_logs
+:Hostname: search1
+:Pid: 23678
+:UUID: a1ee8eb0-e60b-4b81-a143-5dfe9a64b75b
+:Logger: SphinxQuery
+:Payload: 
+:EnvVersion:
+:Severity: 7
+:Fields:
+    | name:"total-matches" value_type:DOUBLE value_double:7352
+    | name:"query_type" value_string:"plain"
+    | name:"wall-time" value_type:DOUBLE value_double:0.011
+    | name:"real-time" value_type:DOUBLE value_double:0.011
+    | name:"offset" value_type:DOUBLE value_double:0
+    | name:"limit" value_type:DOUBLE value_double:100
+    | name:"match-mode" value_string:"ext"
+    | name:"sort-mode" value_string:"ext"
+    | name:"filters-count" value_type:DOUBLE value_double:9
+    | name:"index-names" value_string:"realty_arenda realty_arenda_delta"
+    | name:"query_plain" value_string:"query text"
+    | name:"groupby-attr" value_string:"_id_city_district"
+    | name:"io_stats" value_string:"ios=20 kb=24.0 ioms=0.200 cpums=10.2 agents=(0.003, 1.123)"
 --]]
 
 local dt	= require "date_time"
@@ -80,48 +81,56 @@ l.locale(l)
 
 local msg_type	= read_config("type")
 
-local msg_fields = {}
--- common fields
-msg_fields['query_type'] = nil
-msg_fields['real-time'] = nil
-msg_fields['wall-time'] = nil
-msg_fields['total-matches'] = nil
--- only for plain format
-msg_fields['match-mode'] = nil
-msg_fields['filters-count'] = nil
-msg_fields['sort-mode'] = nil
-msg_fields['offset'] = nil
-msg_fields['limit'] = nil
-msg_fields['groupby-attr'] = nil
-msg_fields['index-names'] = nil
-msg_fields['query_plain'] = nil
--- only for SphinxQL format
-msg_fields['connection_id'] = nil
-msg_fields['query_sql'] = nil
-
 local msg = {
 	Timestamp	= nil,
 	Type		= msg_type,
 	Pid		= nil,
 	Payload		= nil,
-	Fields		= msg_fields
+	Fields		= {}
 }
 
 local number = l.digit^1
 local seconds_float = number * l.P(".") * number
 
-local timestamp = l.Cg( dt.build_strftime_grammar("%a %b %e %f %Y") / dt.time_to_ns, "timestamp" )
+local ts_weekday = l.P"Mon" + "Tue" + "Wed" + "Thu" + "Fri" + "Sat" + "Sun"
+local timestamp = l.Cg( l.Ct( ts_weekday * l.P(" ") * dt.date_mabbr * l.P(" ") * dt.date_mday_sp * l.P(" ") * dt.rfc3339_partial_time * l.P(" ") * dt.date_fullyear ) / dt.time_to_ns, "timestamp" )
+
 local plain_match_mode = l.P("all") + l.P("any") + l.P("phr") + l.P("bool") + l.P("ext")*l.P("2")^-1 + l.P("scan")
 local plain_sort_mode = l.P("rel") + l.P("attr-") + l.P("attr+") + l.P("tsegs") + l.P("ext")
-local plain_query_header = l.Cg(seconds_float, "real-time" ) * l.P(" sec ") * ( l.Cg(seconds_float, "wall-time" ) * l.P(" sec ") )^-1 * l.P("[") * l.Cg(plain_match_mode, "match-mode") * l.P("/") * l.Cg(number, "filters-count") * l.P("/") * l.Cg(plain_sort_mode, "sort-mode") * l.P(" ") * l.Cg(number, "total-matches") * l.P(" (") * l.Cg(number, "offset") * l.P(",") * l.Cg(number, "limit") * l.P(")") * ( l.P(" @") * l.Cg((l.alpha+l.digit)^1, "groupby-attr") )^-1 * l.P("] [") * l.Cg((l.alpha+l.space+l.digit+l.S("-_,"))^1, "index-names") * l.P("]") * l.P(" ")^-1
-local plain_query_grammar = l.Ct( l.P("[") * timestamp * l.P("] ") * plain_query_header * l.Cg( (l.P(1)-l.P("\n"))^0, "query_plain" ) )
-local sql_query_header = l.P(".")
-local sql_query_grammar = l.Ct( l.P("/* ") * timestamp * sql_query_header * l.P("*/ ") * l.Cg( (l.P(1)-l.P("\n"))^0, "query_sql" ) )
+local plain_query_header_times = l.Cg(seconds_float, "real-time" ) * l.P(" sec ") * ( l.Cg(seconds_float, "wall-time" ) * l.P(" sec ") )^-1 * ( l.P("x") * l.Cg(l.digit, "query_multiplier") * l.P(" ") )^-1 
+local plain_query_header_stats = l.P("[") * l.Cg(plain_match_mode, "match-mode") * l.P("/") * l.Cg(number, "filters-count") * l.P("/") * l.Cg(plain_sort_mode, "sort-mode") * l.P(" ") * l.Cg(number, "total-matches") * l.P(" (") * l.Cg(number, "offset") * l.P(",") * l.Cg(number, "limit") * l.P(")") * ( l.P(" @") * l.Cg((l.alpha+l.space+l.digit+l.S("-_,"))^1, "groupby-attr") )^-1 * l.P("]")
+local plain_query_header_indexes =  l.P(" [") * l.Cg((l.alpha+l.space+l.digit+l.S("-_,"))^1, "index-names") * l.P("]")
+local plain_query_header_io = ( l.P(" [") * l.Cg( (l.P(1)-(l.P("]")))^0 , "io_stats") * l.P("]") )^-1
+local plain_query_grammar = l.Ct( l.P("[") * timestamp * l.P("] ") * plain_query_header_times * plain_query_header_stats * plain_query_header_indexes * plain_query_header_io * l.P(" ")^-1 * l.Cg( (l.P(1)-l.P("\n"))^0, "query_plain" ) )
+
+local sql_query_header = l.P(" conn ") * l.Cg( l.digit^1, "connection_id") * (l.P(" real ") * l.Cg( seconds_float, "real-time"))^-1 * l.P(" wall ") * l.Cg( seconds_float, "wall-time") * l.P(" found ") * l.Cg( l.digit^1, "total-matches") * l.P(" ")
+local sql_query_footer_error = (l.P(" ") * (l.P("/*")+l.P("#")) * l.P(" error=") * l.Cg((l.P(1)-(l.P("\n")+l.P(" #")+l.P(" */")))^0, "query_error") * l.P(" */")^-1 )^-1
+local sql_query_footer_io = ( l.P(" ") * (l.P("/* ")+l.P("# ")) * l.Cg((l.P(1)-(l.P("\n")+l.P(" */")))^0, "io_stats") * l.P(" */")^-1 )^-1 * l.Cg( (l.P(1)-l.P("\n"))^0, "rest" )
+local sql_query_grammar = l.Ct( l.P("/* ") * timestamp * sql_query_header * l.P("*/ ") * l.Cg( (l.P(1)-l.P(";"))^0, "query_sql" ) * l.P(";") * sql_query_footer_error * sql_query_footer_io )
 
 function process_message ()
 	local log = read_message("Payload")
 
 	local tmp
+	-- common fields
+	msg.Fields['query_type'] = nil
+	msg.Fields['real-time'] = nil
+	msg.Fields['wall-time'] = nil
+	msg.Fields['total-matches'] = nil
+	msg.Fields['io_stats'] = nil
+	-- only for plain format
+	msg.Fields['match-mode'] = nil
+	msg.Fields['filters-count'] = nil
+	msg.Fields['sort-mode'] = nil
+	msg.Fields['offset'] = nil
+	msg.Fields['limit'] = nil
+	msg.Fields['groupby-attr'] = nil
+	msg.Fields['index-names'] = nil
+	msg.Fields['query_plain'] = nil
+	-- only for SphinxQL format
+	msg.Fields['connection_id'] = nil
+	msg.Fields['query_sql'] = nil
+	msg.Fields['query_error'] = nil
 
 	tmp = plain_query_grammar:match(log)
 	if tmp then
@@ -138,6 +147,7 @@ function process_message ()
 		msg.Fields['limit'] = tonumber(tmp['limit'])
 		msg.Fields['groupby-attr'] = tmp['groupby-attr']
 		msg.Fields['index-names'] = tmp['index-names']
+		msg.Fields['io_stats'] = tmp['io_stats']
 		msg.Fields['query_plain'] = tmp['query_plain']
 		inject_message(msg)
 		return 0
@@ -152,6 +162,9 @@ function process_message ()
 			msg.Fields['total-matches'] = tonumber(tmp['total-matches'])
 			msg.Fields['connection_id'] = tonumber(tmp['connection_id'])
 			msg.Fields['query_sql'] = tmp['query_sql']
+			msg.Fields['query_error'] = tmp['query_error']
+			msg.Fields['io_stats'] = tmp['io_stats']
+--			msg.Fields['rest'] = tmp['rest']
 			inject_message(msg)
 			return 0
 		else
